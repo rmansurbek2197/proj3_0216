85f23a8037da479ee1b5c65200113bebfb821bc9
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-02-16 19:20:15 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# 1
+matn = input()
+print(matn[0])
+
+# 2
+matn = input()
+print(matn[-1])
+
+# 3
+matn = input()
+print(matn.isdigit())
+
+# 4
+matn = input()
+print(matn.isalpha())
+
+# 5
+matn = input()
+print(matn.islower())
